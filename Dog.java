5a40d9a2094367e689a1677a6b657d732e2e8438
Sokos6 <will@comicbooked.com>
2015-12-12 22:31:42 -0700

    first commit

---
+++ Dog.java
@@ -0,0 +1,47 @@
+package WebFrequency;
+
+public class Dog {
+	
+	String name;
+	
+	public static void main(String[] args) {
+		// make a dog object and access it
+		Dog dog1 = new Dog();
+		dog1.bark();
+		dog1.name = "Freddie";
+		
+		// Now make a dog array
+		Dog[] myDogs = new Dog[3];
+		// and put some dogs in it
+		
+		myDogs[0] = new Dog();
+		myDogs[1] = new Dog();
+		myDogs[2] = dog1;
+		
+		//now access the Dogs using the array
+		//references
+		myDogs[0].name = "Doc";
+		myDogs[1].name = "Sheebs";
+		
+		//What is myDogs[2] name? 
+		
+		System.out.print("last dog's name is ");
+		System.out.println(myDogs[2].name);
+		
+		//now loop through the array
+		//and tell all the dogs to bark
+		
+		int x = 0;
+		while(x < myDogs.length) {
+			myDogs[x].bark();
+			x = x + 1;
+		}
+		
+	}
+
+	public void bark() {
+		System.out.println(name + " says Ruff!");
+	}
+	public void eat() { }
+	public void chaseCat() { }
+}
